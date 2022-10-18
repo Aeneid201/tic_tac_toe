422d100266ef6dbd9bbbd19daebf57e6d73269d7
Aeneid201 <aloubi.oumaima@gmail.com>
2022-10-17 20:10:40 -0400

    Fixed players turn to click

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 "use strict";
 
+// Player
+let Player = (name, marker = "x") => {
+  //   let { test } = gameBoard;
+  return { marker };
+};
+
+let player1 = Player("Ouma", "x");
+let player2 = Player("Odin", "o");
+
 let gameBoardModule = (function () {
-  let gameboard = ["x", "o", "x", "x", "x", "o", "x", "o", "o"];
+  let gameboard = [];
+  let winning_combos = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [2, 5, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
 
   // cache the DOM
   let singleGrid = document.querySelectorAll(".singleGrid");
@@ -35,15 +53,27 @@ let gameBoardModule = (function () {
     btn_o.classList.remove("selected");
   });
 
-  // play function
-  function play(e) {
-    if (e.target.innerHTML) {
-      // spot is already marked
-      console.log("Sorry! This spot is already marked.");
-    } else {
-      // this spot can be marked
-      e.target.innerHTML = "x";
-    }
+  let count = 0;
+  let current_marker = player1.marker;
+  for (let i = 0; i < singleGrid.length; i++) {
+    singleGrid[i].addEventListener("click", function () {
+      if (count % 2 !== 0) {
+        current_marker = player2.marker;
+        singleGrid[i].innerHTML = current_marker;
+        btn_x.classList.add("selected");
+        btn_o.classList.remove("selected");
+      } else {
+        current_marker = player1.marker;
+        singleGrid[i].innerHTML = current_marker;
+        btn_o.classList.add("selected");
+        btn_x.classList.remove("selected");
+      }
+
+      let clicked_index = indexInParent(singleGrid[i]);
+      gameboard[clicked_index] = current_marker;
+      // check for winning combos
+      count++;
+    });
   }
 
   // reset the game
@@ -52,20 +82,22 @@ let gameBoardModule = (function () {
     render();
     for (let i = 0; i < singleGrid.length; i++) {
       singleGrid[i].innerHTML = "";
+
+      // reset count of clicks
+      count = 0;
     }
   }
 
   // return
-  return {
-    reset,
-  };
+  return {};
 })();
 
-// Player
-let Player = (name, marker = "x") => {
-  //   let { test } = gameBoard;
-  return { marker };
-};
-
-let player1 = Player("Ouma", "x");
-let player2 = Player("Odin", "o");
+function indexInParent(node) {
+  let children = node.parentNode.childNodes;
+  let num = 0;
+  for (let i = 0; i < children.length; i++) {
+    if (children[i] == node) return num;
+    if (children[i].nodeType == 1) num++;
+  }
+  return -1;
+}
